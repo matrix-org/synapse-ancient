f02cba27a98c3ef0881439dbcb607dc5a3bdf9ed
Kegan Dougal <kegan.dougal@openmarket.com>
2014-07-07 10:35:34 +0100

    Implemented url args to event streaming dict.

--- synapse/api/events.py
+++ synapse/api/events.py
@@ -27,8 +27,10 @@ class BaseEvent(object):
     """ A Synapse REST Event.
     """
 
-    def get_pattern(self):
-        """ Get the regex path pattern to match.
+    @classmethod
+    def get_pattern(cls):
+        """ Get the regex path pattern to match. This should be defined by
+        subclasses.
 
         Returns:
             A regex compiled pattern.
@@ -41,8 +43,8 @@ class BaseEvent(object):
 
     @staticmethod
     def get_valid_json(content, required_keys):
-        """ Check if the content contains the required keys and return the
-        content as JSON.
+        """ Utility method to check if the content contains the required keys
+        and return the content as JSON.
 
         Args:
             content : The raw HTTP content
@@ -61,7 +63,7 @@ class BaseEvent(object):
 
     @staticmethod
     def error(msg, code=0, **kwargs):
-        """ Construct an error response.
+        """ Utility method for constructing an error response.
 
         Args:
             msg : The error message.
@@ -82,7 +84,8 @@ class PutEventMixin(object):
 
     def register(self, http_server):
         super(PutEventMixin, self).register(http_server)
-        http_server.register_path("PUT", self.get_pattern(), self.on_PUT)
+        http_server.register_path("PUT", self.__class__.get_pattern(),
+                                  self.on_PUT)
 
     def on_PUT(self, request, groups):
         raise NotImplementedError("on_PUT callback not implemented")
@@ -94,7 +97,8 @@ class GetEventMixin(object):
 
     def register(self, http_server):
         super(GetEventMixin, self).register(http_server)
-        http_server.register_path("GET", self.get_pattern(), self.on_GET)
+        http_server.register_path("GET", self.__class__.get_pattern(),
+                                  self.on_GET)
 
     def on_GET(self, request, groups):
         raise NotImplementedError("on_GET callback not implemented")
@@ -109,18 +113,62 @@ class EventStreamMixin(object):
     specifying a type. This mixin provides these operations, provided all the
     required data is specified.
      """
+    _ev_pattern = None
 
     def get_event_type(self):
         """ Specify the namespaced event type. """
         raise NotImplementedError()
 
+    def get_event_stream_dict(self, *url_args, **kwargs):
+        """ Constructs a dict which can be streamed as event JSON.
 
-class RoomTopicEvent(PutEventMixin, GetEventMixin, BaseEvent):
+        Args:
+            url_args : The matched groups from the URL.
+            kwargs : Additional keys to add to the event dict.
+        Returns:
+            An event streamble dict.
+        Raises:
+            KeyError if there are unnamed matched groups, as the names are used
+            as the keys for the dict.
+        """
+        event = {}
+        # set additional keys first so we clobber correctly.
+        for key, value in kwargs.iteritems():
+            event[key] = value
 
-    def get_pattern(self):
-        return re.compile("^/rooms/([^/]*)$")
+        event["type"] = self.get_event_type()
 
-    def on_PUT(self, request, groups):
+        # set url args based on the group name and complain if there is no name
+        if not self._ev_pattern:
+            self._ev_pattern = self.__class__.get_pattern()
+            if len(self._ev_pattern.groupindex) != self._ev_pattern.groups:
+                raise KeyError("Event pattern has unnamed groups.")
+
+        # the url_args in http_server match up to the pattern for the url, so
+        # we can just index into url_args and use the group name from the
+        # pattern. Ideally, http_server would return the raw SRE_Pattern which
+        # would preserve group names, but we don't currently.
+        for group_name, group_pos in self._ev_pattern.groupindex.iteritems():
+            # group pos starts at 1, and *url_args are actually a group tuple,
+            # hence needing to index again to the first element
+            event[group_name] = url_args[group_pos - 1][0]
+        return event
+
+
+class RoomTopicEvent(EventStreamMixin, PutEventMixin, GetEventMixin, BaseEvent):
+
+    @classmethod
+    def get_pattern(cls):
+        return re.compile("^/rooms/(?P<roomid>[^/]*)$")
+
+    def get_event_type(self):
+        return "sy.room.topic"
+
+    def on_GET(self, request, *url_args):
+        print "dict: %s" % self.get_event_stream_dict(url_args)
+        return (200, {"rooms": "None"})
+
+    def on_PUT(self, request, *url_args):
         try:
             print BaseEvent.get_valid_json(request.content.read(), ["name"])
         except ValueError:
