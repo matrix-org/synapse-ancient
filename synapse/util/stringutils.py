924742cc707d7601bf33b49bc6e43a25b7e8e4a4
Kegan Dougal <kegan.dougal@openmarket.com>
2014-07-09 10:51:01 +0100

    Implemented auth via access tokens. Clients must POST /register {} to get an access_token which then must be used on all subsequent requests.

--- synapse/util/stringutils.py
+++ synapse/util/stringutils.py
@@ -1,5 +1,10 @@
 # -*- coding: utf-8 -*-
+import string, random
 
 
 def origin_from_ucid(ucid):
     return ucid.split("@", 1)[1]
+
+
+def random_string(length):
+    return ''.join(random.choice(string.ascii_letters) for _ in xrange(length))
