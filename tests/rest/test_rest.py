1780648b89a709fe3e639382b7fba438767a7a42
Paul "LeoNerd" Evans <leonerd@leonerd.org.uk>
2014-07-11 16:01:19 +0100

    Move MockHttpServer into its own file for tests to reuse

--- tests/rest/test_rest.py
+++ tests/rest/test_rest.py
@@ -12,10 +12,11 @@ from synapse.persistence import read_schema
 from synapse.util.http import HttpServer
 
 # python imports
-from mock import patch, Mock
 import json
 import os
 import sqlite3
+
+from ..utils import MockHttpServer
 
 
 class MessageTestCase(unittest.TestCase):
@@ -142,44 +143,3 @@ class MockRegisteredUserModule():
 
     def get_user_by_token(self, token):
         return self.user_id
-
-
-class MockHttpServer(HttpServer):
-
-    callbacks = []  # 3-tuple of method/pattern/function
-
-    @patch('twisted.web.http.Request')
-    @defer.inlineCallbacks
-    def trigger(self, http_method, path, content, mock_request):
-        """ Fire an HTTP event.
-
-        Args:
-            http_method : The HTTP method
-            path : The HTTP path
-            content : The HTTP body
-            mock_request : Mocked request to pass to the event so it can get
-                           content.
-        Returns:
-            A tuple of (code, response)
-        Raises:
-            KeyError If no event is found which will handle the path.
-        """
-
-        # annoyingly we return a twisted http request which has chained calls
-        # to get at the http content, hence mock it here.
-        mock_content = Mock()
-        config = {'read.return_value': content}
-        mock_content.configure_mock(**config)
-        mock_request.content = mock_content
-        for (method, pattern, func) in self.callbacks:
-            if http_method != method:
-                continue
-
-            matcher = pattern.match(path)
-            if matcher:
-                (code, response) = yield func(mock_request, *matcher.groups())
-                defer.returnValue((code, response))
-        raise KeyError("No event can handle %s" % path)
-
-    def register_path(self, method, path_pattern, callback):
-        self.callbacks.append((method, path_pattern, callback))
