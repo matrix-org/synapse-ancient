21ef81a71b59cb45000cadea98a7d7a6ae98cbb9
Erik Johnston <erik.johnston@openmarket.com>
2014-07-17 16:01:28 +0100

    Remove unused import

--- synapse/federation/persistence.py
+++ synapse/federation/persistence.py
@@ -7,11 +7,6 @@ These actions are mostly only used by the :py:mod:`.replication` module.
 
 from twisted.internet import defer
 
-from synapse.persistence.transactions import (
-    TransactionQueries, PduQueries,
-    StateQueries, run_interaction
-)
-
 from .units import Pdu
 
 from synapse.util.logutils import log_function
