3556f7dc3086cff95c04506bda51ba3222011e83
Erik Johnston <erik.johnston@openmarket.com>
2014-07-22 11:42:42 +0100

    Remove unused import

--- synapse/app/homeserver.py
+++ synapse/app/homeserver.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from synapse.persistence import read_schema
-from synapse.federation import ReplicationHandler
 
 from synapse.server import HomeServer
 
