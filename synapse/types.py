a7fe01b1ae7cf8b070e28e073371aace95e13e2d
Paul "LeoNerd" Evans <leonerd@leonerd.org.uk>
2014-07-23 14:36:58 +0100

    Nicer exception message strings when ID/Name string parsing fails

--- synapse/types.py
+++ synapse/types.py
@@ -4,13 +4,6 @@ def split_localpart_domain(s, sigil):
     """Splits a string of the form "{SIGIL}localpart:domain" into its local
     and domain parts and returns them. Requires that the first character of
     the string be the given sigil."""
-    if s[0] != sigil:
-        raise ValueError("Expected '%s' to start with '%s'" % (s, sigil))
-
-    parts = s[1:].split(':')
-    if len(parts) != 2:
-        raise ValueError("Expected '%s' of the form '%slocalname:domain'" %
-                (s, sigil))
 
     return parts[0], parts[1]
 
@@ -35,12 +28,21 @@ class DomainSpecificString(object):
     @classmethod
     def from_string(cls, s, hs):
         """Parse the string given by 's' into a structure object."""
-        localpart, domain = split_localpart_domain(s, cls.SIGIL)
+        if s[0] != cls.SIGIL:
+            raise ValueError("Expected %s string to start with '%s'" %
+                    (cls.__name__, cls.SIGIL))
+
+        parts = s[1:].split(':')
+        if len(parts) != 2:
+            raise ValueError("Expected %s of the form '%slocalname:domain'" %
+                    (cls.__name__, cls.SIGIL))
+
+        domain = parts[1]
 
         # This code will need changing if we want to support multiple domain
         # names on one HS
         is_mine = domain == hs.hostname
-        return cls(localpart=localpart, domain=domain, is_mine=is_mine)
+        return cls(localpart=parts[0], domain=domain, is_mine=is_mine)
 
     def to_string(self):
         """Return a string encoding the fields of the structure object."""
