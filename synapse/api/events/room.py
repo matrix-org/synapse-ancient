f9532bd5502303e0065433054722ee14d32bc7d3
Erik Johnston <erik.johnston@openmarket.com>
2014-07-23 15:35:34 +0100

    Fix RoomMemberEvent to understand state_key

--- synapse/api/events/room.py
+++ synapse/api/events/room.py
@@ -18,10 +18,12 @@ class RoomMemberEvent(SynapseEvent):
 
     valid_keys = SynapseEvent.valid_keys + [
         "target_user_id",  # target
+        "membership",  # action
     ]
 
     def __init__(self, **kwargs):
-        kwargs["state_key"] = kwargs["target_user_id"]
+        if "target_user_id" in kwargs:
+            kwargs["state_key"] = kwargs["target_user_id"]
         super(RoomMemberEvent, self).__init__(**kwargs)
 
     def get_content_template(self):
