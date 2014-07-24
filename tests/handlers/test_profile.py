10007658c8ec324e149af4ccac3f064ad84038b9
Paul "LeoNerd" Evans <leonerd@leonerd.org.uk>
2014-07-24 15:12:25 +0100

    Test the ProfileHandler at the actual handlers level without Rest API over he top

---
+++ tests/handlers/test_profile.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+
+from twisted.trial import unittest
+from twisted.internet import defer
+
+from mock import Mock
+
+from synapse.api.auth import Auth
+from synapse.api.errors import AuthError
+from synapse.server import HomeServer
+
+myid = "!1234ABCD:test"
+
+class ProfileTestCase(unittest.TestCase):
+    """ Tests profile management. """
+
+    def setUp(self):
+        self.db_pool = Mock(spec=["runInteraction"])
+
+        hs = HomeServer("test",
+                db_pool=self.db_pool)
+
+        self.frank = hs.parse_userid("!1234ABCD:test")
+
+        self.handlers = hs.get_handlers()
+
+    @defer.inlineCallbacks
+    def test_get_my_name(self):
+        mocked_ri = self.db_pool.runInteraction
+        mocked_ri.return_value = defer.succeed("Frank")
+
+        displayname = yield self.handlers.profile_handler.get_displayname(
+                self.frank)
+
+        self.assertEquals("Frank", displayname)
+        # Can't easily assert on the [0] positional argument as it's a callable
+        self.assertEquals(mocked_ri.call_args[0][1], "1234ABCD")
+
+    @defer.inlineCallbacks
+    def test_set_my_name(self):
+        mocked_ri = self.db_pool.runInteraction
+        mocked_ri.return_value = defer.succeed(())
+
+        yield self.handlers.profile_handler.set_displayname(
+                self.frank, "1234ABCD", "Frank Jr.")
+
+        self.assertEquals(mocked_ri.call_args[0][1], "1234ABCD")
+        self.assertEquals(mocked_ri.call_args[0][2], "Frank Jr.")
+
+    @defer.inlineCallbacks
+    def test_set_my_name_noauth(self):
+        d = self.handlers.profile_handler.set_displayname(self.frank, "4567",
+                "Frank Jr.")
+
+        yield self.assertFailure(d, AuthError)
--- tests/rest/test_profiles.py
+++ tests/rest/test_profiles.py
@@ -8,7 +8,7 @@ from mock import Mock
 
 from ..utils import MockHttpServer
 
-from synapse.api.auth import Auth
+from synapse.api.errors import SynapseError, AuthError
 from synapse.server import HomeServer
 
 myid = "!1234ABCD:test"
@@ -21,42 +21,49 @@ class ProfilesTestCase(unittest.TestCase):
 
     def setUp(self):
         self.mock_server = MockHttpServer()
-        self.db_pool=Mock(spec=["runInteraction"])
+        self.mock_handler = Mock(spec=[
+            "get_displayname",
+            "set_displayname",
+        ])
 
         hs = HomeServer("test",
-                db_pool=self.db_pool,
+                db_pool=None,
                 http_server=self.mock_server)
         hs.get_auth().get_user_by_token = self.mock_get_user_by_token
+        hs.get_handlers().profile_handler = self.mock_handler
 
         hs.register_servlets()
 
     @defer.inlineCallbacks
     def test_get_my_name(self):
-        mocked_ri = self.db_pool.runInteraction
-        mocked_ri.return_value = defer.succeed("Frank")
+        mocked_get = self.mock_handler.get_displayname
+        mocked_get.return_value = defer.succeed("Frank")
 
         (code, response) = yield self.mock_server.trigger("GET",
                 "/profile/%s/displayname" % (myid), None)
 
         self.assertEquals(200, code)
         self.assertEquals("Frank", response)
-        self.assertEquals(mocked_ri.call_args[0][1], "1234ABCD")
+        self.assertEquals(mocked_get.call_args[0][0].localpart, "1234ABCD")
 
     @defer.inlineCallbacks
     def test_set_my_name(self):
-        mocked_ri = self.db_pool.runInteraction
-        mocked_ri.return_value = defer.succeed(())
+        mocked_set = self.mock_handler.set_displayname
+        mocked_set.return_value = defer.succeed(())
 
         (code, response) = yield self.mock_server.trigger("PUT",
                 "/profile/%s/displayname" % (myid), '"Frank Jr."')
 
         self.assertEquals(200, code)
-        # Can't easily assert on the [0] positional argument as it's a callable
-        self.assertEquals(mocked_ri.call_args[0][1], "1234ABCD")
-        self.assertEquals(mocked_ri.call_args[0][2], "Frank Jr.")
+        self.assertEquals(mocked_set.call_args[0][0].localpart, "1234ABCD")
+        self.assertEquals(mocked_set.call_args[0][1], "1234ABCD")
+        self.assertEquals(mocked_set.call_args[0][2], "Frank Jr.")
 
     @defer.inlineCallbacks
     def test_set_my_name_noauth(self):
+        mocked_set = self.mock_handler.set_displayname
+        mocked_set.side_effect = AuthError(400, "message")
+
         (code, response) = yield self.mock_server.trigger("PUT",
                 "/profile/%s/displayname" % ("!4567:test"), '"Frank Jr."')
 
@@ -65,6 +72,9 @@ class ProfilesTestCase(unittest.TestCase):
 
     @defer.inlineCallbacks
     def test_get_other_name(self):
+        mocked_get = self.mock_handler.get_displayname
+        mocked_get.return_value = defer.succeed("Bob")
+
         (code, response) = yield self.mock_server.trigger("GET",
                 "/profile/%s/displayname" % ("!opaque:elsewhere"), None)
 
@@ -73,6 +83,9 @@ class ProfilesTestCase(unittest.TestCase):
 
     @defer.inlineCallbacks
     def test_set_other_name(self):
+        mocked_set = self.mock_handler.set_displayname
+        mocked_set.side_effect = SynapseError(400, "message")
+
         (code, response) = yield self.mock_server.trigger("PUT",
                 "/profile/%s/displayname" % ("!opaque:elsewhere"), None)
 
