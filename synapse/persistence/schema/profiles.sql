d8894c1e0363c8cabffea92545ffe39378a1290b
Paul "LeoNerd" Evans <leonerd@leonerd.org.uk>
2014-07-25 15:33:10 +0100

    Don't need to insert "Frank" from schema/profile.sql any more

--- synapse/persistence/schema/profiles.sql
+++ synapse/persistence/schema/profiles.sql
@@ -3,6 +3,3 @@ CREATE TABLE IF NOT EXISTS profiles(
     displayname TEXT,
     FOREIGN KEY(user_id) REFERENCES users(id)
 );
-
--- TODO(paul): For unit testing just insert some static data
-INSERT OR IGNORE INTO profiles(user_id, displayname) VALUES (123, "Frank");
