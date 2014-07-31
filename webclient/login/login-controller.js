271ccb83e594eec01741fe108b9f2471584b3465
Emmanuel ROHEE <erohee@amdocs.com>
2014-07-31 13:56:21 +0200

    Made web client work with the home server code of the git repository. The web client does not work anymore with the hacky HS hosted at matrix.openmarket.com (mainly because it mixed user_name & user_id) Changed the login to be able to login with an existing user and his access token

--- webclient/login/login-controller.js
+++ webclient/login/login-controller.js
@@ -5,28 +5,73 @@ angular.module('LoginController', [])
     $scope.state = state.state;
     
     $scope.account = {
-        "server": "http://matrix.openmarket.com",
+        server: "", // http://matrix.openmarket.com", The hacky version of the HS hosted at the this URL does not work anymore
+        user_name: "",
+        homeserver_name: "",
+        access_token: ""
     };
-    $scope.room = "tng";
 
-    //$scope.account.server = "http://localhost:8080";
-    $scope.account.user_id = "Manu";
-    $scope.room = "ManuRoom";
+/*
+    $scope.account.server = "http://localhost:8080";
+    $scope.account.user_name = "Manu10";
+    $scope.account.homeserver_name = "localhost";
+    $scope.account.access_token = "QE1hbnUxMDpsb2NhbGhvc3Q..KbqazxGnAJlibDApAP";
+    */
+    
+    var computeUserId = function() {
+        return "@" + $scope.account.user_name + ":" + $scope.account.homeserver_name;
+    };
 
-    $scope.login = function() {
+    $scope.register = function() {
         var data = {
-          "user_id" : $scope.account.user_id
+          "user_id" : $scope.account.user_name
         };
         $http.post($scope.account.server + "/register", data).
             success(function(data, status, headers, config) {
                 $scope.feedback = "Success";
                 $scope.state.server = $scope.account.server;
                 $scope.state.access_token = data.access_token;
+                $scope.state.user_name = $scope.account.user_name;
                 $scope.state.user_id = data.user_id;
-                $location.path("room/" + $scope.room);
+                
+                 // Go to the user's rooms list page
+                $location.path("rooms");
             }).
             error(function(data, status, headers, config) {
-                $scope.feedback = "Failure: " + data;
+                var reason = data.error;
+                if (!data.error) {
+                    reason = JSON.stringify(data);
+                }
+                $scope.feedback = "Failure: " + reason;
+            });
+    };
+    
+    $scope.login = function() {
+
+        // Validate the token by making a request to the HS
+        $http.get($scope.account.server + "/users/" + computeUserId() + "/rooms/list", {
+            "params": {
+                "access_token" : $scope.account.access_token
+            }}).
+            success(function(data, status, headers, config) {
+                
+                // The request passes. We can consider to be logged in
+                $scope.feedback = "Success";
+
+                $scope.state.server = $scope.account.server;
+                $scope.state.access_token = $scope.account.access_token;
+                $scope.state.user_name = $scope.account.user_name ;  
+                $scope.state.user_id = computeUserId();  
+
+                // Go to the user's rooms list page
+                $location.path("rooms");
+            }).
+            error(function(data, status, headers, config) {
+                var reason = data.error;
+                if (!data.error) {
+                    reason = JSON.stringify(data);
+                }
+                $scope.feedback = "Failure: " + reason;
             });
     };
 }]);
