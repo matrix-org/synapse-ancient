0e20fde555c02bfed7d9180370b64c985c3c87c4
Erik Johnston <erik.johnston@openmarket.com>
2014-08-01 09:35:26 +0100

    Add prev state edges

--- graph/graph.py
+++ graph/graph.py
@@ -80,9 +80,21 @@ def make_graph(pdus, room, filename_prefix):
 
             if end_name not in node_map:
                 print "%s not in nodes" % end_name
+                continue
 
             edge = pydot.Edge(node_map[start_name], node_map[end_name])
             graph.add_edge(edge)
+
+        # Add prev_state edges, if they exist
+        if pdu.get("prev_state_id") and pdu.get("prev_state_origin"):
+            prev_state_name = make_name(
+                pdu.get("prev_state_id"), pdu.get("prev_state_origin")
+            )
+            state_edge = pydot.Edge(
+                node_map[start_name], node_map[prev_state_name],
+                style='dotted'
+            )
+            graph.add_edge(state_edge)
 
     graph.write('%s.dot' % filename_prefix, format='raw', prog='dot')
     graph.write_png("%s.png" % filename_prefix, prog='dot')
