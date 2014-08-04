4e6e88d56509bc8ee8faa73019b0d5efef124190
Paul "LeoNerd" Evans <leonerd@leonerd.org.uk>
2014-08-04 13:47:37 +0100

    Fix test I just broke wrt. presence state of offline users

--- tests/handlers/test_presence.py
+++ tests/handlers/test_presence.py
@@ -279,7 +279,8 @@ class PresenceInvitesTestCase(unittest.TestCase):
         presence = yield self.handlers.presence_handler.get_presence_list(
                 observer_user=self.u_apple)
 
-        self.assertEquals([{"observed_user": self.u_banana}], presence)
+        self.assertEquals([{"observed_user": self.u_banana,
+                            "state": OFFLINE}], presence)
 
         self.datastore.get_presence_list.assert_called_with("apple",
                 accepted=None)
@@ -292,7 +293,8 @@ class PresenceInvitesTestCase(unittest.TestCase):
         presence = yield self.handlers.presence_handler.get_presence_list(
                 observer_user=self.u_apple, accepted=True)
 
-        self.assertEquals([{"observed_user": self.u_banana}], presence)
+        self.assertEquals([{"observed_user": self.u_banana,
+                            "state": OFFLINE}], presence)
 
         self.datastore.get_presence_list.assert_called_with("apple",
                 accepted=True)
